657458ec4ce6e74ca88cc103274281cee56b4e7b
SergienkoNikita <SNikita312@yandex.ru>
2023-05-30 00:53:39 +0300

    add formik in project

--- src/features/authentication/login-form/login-form.tsx
+++ src/features/authentication/login-form/login-form.tsx
@@ -1,45 +1,44 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import { FormikProps, useFormik } from 'formik';
 import { BaseInput } from '../../../shared/ui';
 
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
+
 export const LoginForm: FC = () => {
-  const [loginForm, setLoginForm] = useState<{
-    login: string | number;
-    password: string | number;
-  }>({
-    login: '',
-    password: '',
+  const formik: FormikProps<LoginFormValues> = useFormik<LoginFormValues>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (e) => {
+      console.log(e);
+    },
   });
 
-  const setLogin = (value: string | number) => {
-    setLoginForm({ ...loginForm, login: value });
-  };
-
-  const setPassword = (value: string | number) => {
-    setLoginForm({ ...loginForm, password: value });
-  };
-
-  const log = () => {
-    // eslint-disable-next-line no-console
-    console.log(loginForm);
-  };
   return (
-    <div style={{ width: '60%' }}>
+    <form onSubmit={formik.handleSubmit} style={{ width: '60%' }}>
       <BaseInput
         type='text'
         placeholder='Логин'
-        value={loginForm.login}
-        onChange={setLogin}
+        name='email'
+        value={formik.values.email}
+        onChange={(value) => formik.setFieldValue('email', value)}
         style={{ marginBottom: 10 }}
+        clearable
       />
 
       <BaseInput
         type='password'
+        name='password'
         placeholder='Пароль'
-        value={loginForm.password}
-        onChange={setPassword}
+        value={formik.values.password}
+        onChange={(value) => formik.setFieldValue('password', value)}
         style={{ marginBottom: 10 }}
       />
-      <button onClick={log}>показать логин</button>
-    </div>
+      <button type='submit'>показать логин</button>
+    </form>
   );
 };
