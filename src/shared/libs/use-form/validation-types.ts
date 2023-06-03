96fe149d775dcd3273f1eb687d1f1872a830343d
SergienkoNikita <SNikita312@yandex.ru>
2023-06-03 19:48:37 +0300

    fix comment

--- src/shared/libs/use-form/validation-types.ts
+++ src/shared/libs/use-form/validation-types.ts
@@ -328,9 +328,7 @@ const oneOfValidator = (value: unknown, list: unknown[]): boolean => {
   if (Array.isArray(value)) {
     return value.every((val) => oneOfValidator(val, list));
   }
-  return Array.from(list).some((item) =>
-    // eslint-disable-next-line
-     item == value);
+  return Array.from(list).some((item) => item === value);
 };
 
 defineRule('required', requiredValidator);
